2ca75cee706be2cb10b3427e1c073010e818e4e7
eujingchua <eujingchua@microsoft.com>
2022-04-20 20:57:26 +0000

    Initial test notebook

---
+++ step_scripts/split_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Preprocessing script for the beer dataset
+import pandas as pd
+from typing import List
+
+from zendikon.aml.tabular_data_step_decorator import TabularDataAmlPythonStepCompatible
+from zendikon.aml.types import StepArgument
+from helper import split_full_for_forecasting
+
+
+@TabularDataAmlPythonStepCompatible(
+    step_arguments=[
+        StepArgument(
+            name="time_column_name",
+            helper_message="Time column name for training",
+            data_type=str,
+            default_value="DATE",
+        ),
+        StepArgument(
+            name="split_date",
+            helper_message="Date to split the dataset into train and validation sets",
+            data_type=str,
+            default_value="2012-01-01",
+        )
+    ],
+)
+
+def split_data(data, cli_args=None, run=None) -> List[pd.DataFrame]:
+    train_data = data[data[cli_args.time_column_name] < cli_args.split_date]
+    valid_data = data[data[cli_args.time_column_name] >= cli_args.split_date]
+    return [train_data, valid_data]
+
+if __name__ == "__main__":
+    split_data()
+
+
